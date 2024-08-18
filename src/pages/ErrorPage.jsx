b80b75cbdf26ffe80fff920fe98e1c57ce35722d
Arman <armangulistani@gmail.com>
2024-08-18 12:52:25 -0400

    Completed error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import CallToActionButton from "../components/CallToActionButton";
 import "./styles/ErrorPage.css";
 
 export default function ErrorPage() {
   return (
     <section className="ErrorPage page-appearance-animation">
-      <p>You are not supposed to be here... click the link dummy</p>
-      <Link to="/">Return to home page</Link>
+      <div className="page-container ErrorPage__container">
+        <p className="ErrorPage__error-message">
+          <span style={{ color: "red" }}>Error 404. Page not found.</span> You
+          are not supposed to be here!
+        </p>
+        <p className="ErrorPage__cta-message">
+          But don't worry <span className="gradient-text">beautiful</span>,
+          treasures await you on our shopping page 💍.
+        </p>
+
+        <CallToActionButton link="/shop">Shop now</CallToActionButton>
+      </div>
     </section>
   );
 }
